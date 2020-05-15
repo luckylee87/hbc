48260c4627e60fd82f66a119a8db7c3d69e72c48
xinrongli <254033084@qq.com>
2020-05-15 17:18:45 +0800

    代码生成

--- src/main/java/com/wstro/config/MybatisPlusConfig.java
+++ src/main/java/com/wstro/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.wstro.config;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,4 +22,8 @@ public class MybatisPlusConfig {
 		return paginationInterceptor;
 	}
 
+	@Bean
+	public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+		return new OptimisticLockerInterceptor();
+	}
 }
